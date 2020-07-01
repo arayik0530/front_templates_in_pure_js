daf26911895f1ca7048850c0333a4cac9141d5b3
arayik0530 <arayik0530@gmail.com>
2020-07-01 22:16:41 +0400

    carousel

---
+++ front_templates_in_pure_js/carousel/js/index.js
@@ -0,0 +1,96 @@
+const SEASONCOUNT = 4;
+let slideIndex = 1;
+
+(function () {
+    currentSlide(slideIndex, document.getElementById("first"));
+    document.getElementById("number").innerText = `${slideIndex}/${SEASONCOUNT}`;
+})();
+
+function next(){
+    ++slideIndex;
+    refresh();
+}
+
+function previous(){
+    --slideIndex;
+    refresh();
+}
+
+function refresh() {
+    if(slideIndex < 1) slideIndex = 4;
+    if(slideIndex > 4) slideIndex = 1;
+    document.getElementById("number").innerText = `${slideIndex}/${SEASONCOUNT}`;
+    let dotElement;
+
+    switch (slideIndex) {
+        case 1: {
+            dotElement = document.getElementById("first");
+            break;
+        }
+        case 2: {
+            dotElement = document.getElementById("second");
+            break;
+        }
+        case 3: {
+            dotElement = document.getElementById("third");
+            break;
+        }
+        case 4: {
+            dotElement = document.getElementById("fourth");
+            break;
+        }
+
+        default:
+    }
+
+    currentSlide(slideIndex, dotElement);
+}
+
+function currentSlide(index, dotElement){
+
+    dotElement.classList.add("active");
+    let dots = document.getElementsByClassName("dot");
+    for(let dot of dots){
+        if(dot === dotElement)
+            continue;
+        dot.classList.remove("active");
+    }
+    slideIndex = index;
+
+    let pics = document.getElementsByClassName("imageContainer");
+
+    let currentSeason;
+
+    switch (index) {
+        case 1: {
+            currentSeason = document.getElementById("spring");
+            break;
+        }
+        case 2: {
+            currentSeason = document.getElementById("summer");
+            break;
+        }
+        case 3: {
+            currentSeason = document.getElementById("autumn");
+            break
+        }
+        case 4: {
+            currentSeason = document.getElementById("winter");
+            break;
+        }
+
+        default:
+    }
+
+    for(let pic of pics){
+        pic.style.display = 'none';
+    }
+
+    currentSeason.style.display = 'block';
+
+}
+
+function play() {
+   let timerId = setInterval(() => next(), 1000);
+}
+
